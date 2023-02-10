5386957a7bbabc01585127ae09d124df094b75e3
Marco Antonio Hermida Feria <Marco.A.Hermida@gmail.com>
2023-02-10 13:01:54 -0800

    Imprimir testimonio presentar componente testimonio

--- example-app/src/Componentes/Testimonio.jsx
+++ example-app/src/Componentes/Testimonio.jsx
@@ -4,18 +4,19 @@ function Testimonio(){
   return (
     <div className="contenedor testimonio">
       <img className="imagen-testimonio"
-      src="../imagenes/testimonio-emma.png"/>
+      src={require("../imagenes/testimonio-emma.png")}
+      alt="Foto de Emma"/>
 
-
+      <div className="contenedor-texto-testimonio">
+        <p className="Nombre-testimonio">Emma Bostian en Suecia</p>
+        <p className="cargo-testimonio">Ingeniera de software en Spotify</p>
+        <p className="texto-testimonio">Siempre he tenido pobremas para aprender javascript pero gracias a slapchop ahora my vida is more facinante</p>
+      </div>
 
 
     </div>
-
-
-
-
   );
 
-
-
 }
+
+export default Testimonio;
